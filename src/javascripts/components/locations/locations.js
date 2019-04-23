784f76cada61ed3eecbcf725efdc51c9590468d6
Samuel Trejo <samuel.trejo@s.tcatnashville.edu>
2019-04-22 19:26:42 -0500

    dynamic styling on location headers

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -5,11 +5,33 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
     domString += `<div id="${location.id}" class="card text-center w-15 m-3">`;
-    domString += `<div class="card-header">${location.name}</div>`;
+    domString += `<div class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</div>`;
     domString += '<div class="card-body">';
     domString += `<img src="${location.imageUrl}" class="card-img-top" alt="ball of twine">`;
     domString += `<p class="card-text">${location.address}</p>`;
